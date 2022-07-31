7769c262adc0e24152a33235f78f1e9b82651cd6
Tekina <nm0321@yandex.ru>
2022-07-31 12:39:55 +0300

    delete hash

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,15 +1,13 @@
 const pool = require('../../database'),
     jwt = require('jsonwebtoken'),
-    cipher = require('../../crypto/cipher'),
     decipher = require('../../crypto/decipher'),
     headers = require('../../utils/headers')
 
 
 function loginUser(req, res) {
     const userCheck = `SELECT username, password, id, ${req.body.project} FROM global WHERE username=? LIMIT 1`
-    const loginReg = `SELECT username, blacklist FROM ${req.body.project} WHERE username=? LIMIT 1`
+    const loginReg = `SELECT username, blacklist, role FROM ${req.body.project} WHERE username=? LIMIT 1`
     const registrationProject = `INSERT INTO ${req.body.project} (username,blacklist,hash,role) VALUES ( ?, ?, ?, ? )`
-    const hashCheck = `SELECT hash FROM ${req.body.project} WHERE username=? LIMIT 1`
     const blacklistCheck = `SELECT blacklist FROM ${req.body.project} WHERE username=? LIMIT 1`
     let info = ''
 
@@ -19,18 +17,16 @@ function loginUser(req, res) {
             process.env.TOKEN_KEY, function (err, decoded) {
                 if (!decoded) res.status(404).json({message: 'Token not found'})
                 else {
-                    Promise.all([
-                        pool(hashCheck, [decoded.username]),
                         pool(blacklistCheck, [decoded.username])
-                    ])
                         .then((result) => {
-                            if (result[1].blacklist === 1) res.status(404).json({message: 'User is banned'})
-                            else if (result[0].hash !== null) res.status(200).json({
-                                username: decoded.username,
-                                project: decoded.project,
-                                id: decoded.id
-                            })
-                            else res.status(404).json({message: 'Token invalided'})
+                            if (result[0].blacklist === 1) res.status(404).json({message: 'User is banned'})
+                            else res.status(200)
+                                .json({
+                                    id: decoded.id,
+                                    username: decoded.username,
+                                    project: decoded.project,
+                                    role: decoded.role,
+                                })
                         })
                         .catch(() => {
                             res.status(500)
@@ -43,32 +39,32 @@ function loginUser(req, res) {
                 info = result[0]
                 return Promise.all([
                     decipher(req.body.password, result[0].password),
-                    cipher(result[0].username),
                     pool(loginReg, [result[0].username])
                 ])
             })
             .then((result) => {
-                info.hash = result[1]
-                if (!result[0] || result[2][0]?.blacklist === 1) throw new Error()
-                else if (!result[2][0]?.username) return pool(registrationProject, [req.body.username, 0, result[1], 'user'])
+                info.role = result[1][0]?.role
+                if (!result[0] || result[1][0]?.blacklist === 1) throw new Error()
+                else if (!result[1][0]?.username) return pool(registrationProject, [req.body.username, 0, result[1], 'user'])
                 else return '+'
             })
-            .then(() =>{
+            .then(() => {
                 res.status(200)
                     .set(headers.cache)
                     .json({
-                    id: info.id,
-                    username: info.username,
-                    project: req.body.project,
-                    token: jwt.sign({
                         id: info.id,
                         username: info.username,
-                        hash: info.hash,
                         project: req.body.project,
-                    }, process.env.TOKEN_KEY)
-                })
+                        role: info.role,
+                        token: jwt.sign({
+                            id: info.id,
+                            username: info.username,
+                            project: req.body.project,
+                            role: info.role,
+                        }, process.env.TOKEN_KEY)
+                    })
             })
-            .catch(() => res.status(404).json({message: 'User not found'}))
+            .catch(() => res.status(404).json({message: 'User/password not found'}))
     }
 }
 
--- routes/auth/registration.js
+++ routes/auth/registration.js
@@ -6,29 +6,26 @@ const express = require('express'),
 
 function registration(req, res) {
     const registrationSQL = `INSERT INTO global (username,PASSWORD,email,blacklist,role,${req.body.project}) VALUES ( ?, ?, ?, ?, ?, ? )`
-    const projectReg = `INSERT INTO ${req.body.project} (username,blacklist,hash,role) VALUES ( ?, ?, ?, ? )`
-    let hash = ''
+    const projectReg = `INSERT INTO ${req.body.project} (username,blacklist,role) VALUES ( ?, ?, ? )`
 
     cipher(req.body.password)
         .then((result) => {
             return Promise.all([
                 pool(registrationSQL, [req.body.username, result, req.body.email || null, 0, 'user', 1]),
-                cipher(req.body.username),
+                pool(projectReg, [req.body.username, 0, 'user'])
             ])
         })
+        .then(() => pool(projectReg, [req.body.username, 0, 'user']))
         .then((result) => {
-            hash = result[1]
-            return pool(projectReg, [req.body.username, 0, result[1], 'user'])
-        })
-        .then(() => res.status(200).json({
+            console.log(result)
+            res.status(200).json({
             username: req.body.username,
             project: req.body.project,
             token: jwt.sign({
                 username: req.body.username,
-                hash: hash,
                 project: req.body.project
             }, process.env.TOKEN_KEY),
-        }))
+        })})
         .catch((err) => err ?
             res.status(400)
                 .json({
