b5a87745b594aafc17ca04f2304fbb0d60aa85bf
Tekina <nm0321@yandex.ru>
2022-11-15 08:59:51 +0300

    add uuid generator

--- initialization.js
+++ initialization.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 const globalTable = `CREATE TABLE ${process.env.CORE_TABLE_NAME} (id int NOT NULL AUTO_INCREMENT, username varchar(20) NOT NULL UNIQUE, password varchar(64), role varchar(20), access_token VARCHAR (30), refresh_token VARCHAR (30), PRIMARY KEY(id) )`
 const additionalInfoTable =`CREATE TABLE ${process.env.ADDITIONAL_TABLE_NAME} (id INT, email varchar(20), blacklist boolean, discord_id varchar(20), FOREIGN KEY (id) REFERENCES ${process.env.CORE_TABLE_NAME}(id), PRIMARY KEY(id) )`
-const characterTable = `CREATE TABLE ${process.env.CHARACTER_TABLE_NAME} (id int NOT NULL, character_name varchar(20) NOT NULL UNIQUE, password varchar(64), skin VARCHAR (40), FOREIGN KEY (id) REFERENCES ${process.env.CORE_TABLE_NAME}(id), PRIMARY KEY(character_name) )`
+const characterTable = `CREATE TABLE ${process.env.CHARACTER_TABLE_NAME} (id int NOT NULL, character_name varchar(20) NOT NULL UNIQUE, password varchar(64), skin VARCHAR (40), uuid VARCHAR (40), FOREIGN KEY (id) REFERENCES ${process.env.CORE_TABLE_NAME}(id), PRIMARY KEY(character_name) )`
 
 
 async function init() {
--- routes/character/createCharacter.js
+++ routes/character/createCharacter.js
@@ -1,10 +1,11 @@
 var jwtCheck = require('../../validator/jwtCheck'),
     pool = require('../../database'),
     fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args)),
-    cipher = require('../../crypto/cipher')
+    cipher = require('../../crypto/cipher'),
+    { v4: uuidv4 } = require('uuid')
 
 function createCharacter(req, res) {
-    var character = `INSERT INTO ${process.env.CHARACTER_TABLE_NAME} (id,character_name,password,skin) VALUES ( ?, ?, ?, ? )`
+    var character = `INSERT INTO ${process.env.CHARACTER_TABLE_NAME} (id,character_name,password,skin,uuid) VALUES ( ?, ?, ?, ?, ? )`
     const timeStamp = new Date().getTime()
     var globalInfo = {}
     Promise.all([
@@ -13,6 +14,7 @@ function createCharacter(req, res) {
     ])
         .then((result) => {
             globalInfo = result
+            globalInfo.uuid = uuidv4()
             return fetch(process.env.GAMESYSTEM_URL + '/api', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
@@ -21,6 +23,7 @@ function createCharacter(req, res) {
                     params: {
                         entity_class: 'PlayerEntity',
                         uid: req.body.name,
+                        uuid: globalInfo.uuid,
                         name: req.body.name,
                         display_name: req.body.display_name,
                         entity_type: 'UNKNOWN'
@@ -35,7 +38,7 @@ function createCharacter(req, res) {
         .then((info) => {
             if(info.error?.code) throw info.error
             else return pool(character, [globalInfo[1].id, req.body.name, globalInfo[0],
-                `default.png`])
+                `default.png`, globalInfo.uuid])
         })
         .then(() => res.status(200).json({
             status: 'success'
