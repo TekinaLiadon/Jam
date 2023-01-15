d0130a073130bafb09b571c4bbc5b95a5264c788
Tekina <nm0321@yandex.ru>
2023-01-15 15:14:46 +0300

    test

--- router/Auth/tokenDiscord.js
+++ router/Auth/tokenDiscord.js
@@ -47,6 +47,7 @@ export default {
             })
             .then((result) => {
                 info.insertId = result[0]?.id
+                console.log(info.insertId, result[0])
                 if (result[0]?.username) return connection
                     .query(updateInfo, [info.access_token, info.refresh_token, info.username + info.discriminator])
                 else return connection
